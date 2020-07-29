7b6349f1173070366ab345dcf63cc1ccbd7028f0
Artem <Tkachuk>
2020-07-28 19:23:42 -0700

    Finished the creation of the "I am Rich" app using SwiftUI. Looking forward to building the HackerNews app using SwiftUI!

--- I Am Rich SwiftUI/ContentView.swift
+++ I Am Rich SwiftUI/ContentView.swift
@@ -10,12 +10,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200.0, height: 200.0)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhoneSE"))
     }
 }
